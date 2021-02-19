e980d6bee3b8ad5bd542aa4b353755c527e0a970
Developer Experience team at Tendermint <hello@tendermint.com>
2021-02-19 10:12:09 -0500

    Initialized with Starport

---
+++ x/voter/genesis.go
@@ -0,0 +1,17 @@
+package voter
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ryandines/voter/x/voter/keeper"
+	"github.com/ryandines/voter/x/voter/types"
+)
+
+// InitGenesis initializes the capability module's state from a provided genesis
+// state.
+func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
+}
+
+// ExportGenesis returns the capability module's exported genesis.
+func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
+	return types.DefaultGenesis()
+}
